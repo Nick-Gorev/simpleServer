6d98d0f757829abe550132c7129f558b6daba22d
Nick-Gorev <nickag@bk.ru>
2016-02-21 18:16:06 +0300

    remove comments
    remove comments

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 2222))
-s.listen(10) # очередь - 10 клиентов
+s.listen(10)
 while True:
     conn, addr = s.accept()
     while True:
